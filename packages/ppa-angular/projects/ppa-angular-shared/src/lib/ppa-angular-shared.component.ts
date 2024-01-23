9f3304d09f3719a5f9f600fbdc1eb49e6f9423f1
Federico Camacho <federicocamacho97@gmail.com>
2024-01-23 11:51:11 -0500

    Task: Setup angular shared library (update test, tailwind config and docker)

---
+++ packages/ppa-angular/projects/ppa-angular-shared/src/lib/ppa-angular-shared.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ppa-shared-shared',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      ppa-angular-shared works!
+    </p>
+  `,
+  styles: ``
+})
+export class PpaAngularSharedComponent {
+
+}
---
+++ packages/ppa-angular/projects/ppa-angular-shared/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of ppa-angular-shared
+ */
+
+export * from './lib/index';
